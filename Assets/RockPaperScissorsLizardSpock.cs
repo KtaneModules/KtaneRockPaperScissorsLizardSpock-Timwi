3b55c1bc0188cda60d83257d1fc0e8eb03325874
Timwi <github@timwi.de>
2016-11-10 08:51:30 +0100

    Rock-Paper-Scissors-Lizard-Spock module

---
+++ Assets/RockPaperScissorsLizardSpock.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RockPaperScissorsLizardSpock;
+using UnityEngine;
+using Rnd = UnityEngine.Random;
+
+/// <summary>
+/// On the Subject of Rock-Paper-Scissors-Lizard-Spock
+/// Created by Timwi
+/// </summary>
+public class RockPaperScissorsLizardSpockModule : MonoBehaviour
+{
+    public KMBombInfo Bomb;
+    public KMBombModule Module;
+    public KMAudio Audio;
+    public KMSelectable[] Selectables;
+
+    void Start()
+    {
+        Debug.Log("[Rock-Paper-Scissors-Lizard-Spock] Started");
+        Module.OnActivate += ActivateModule;
+    }
+
+    void ActivateModule()
+    {
+        Debug.Log("[Rock-Paper-Scissors-Lizard-Spock] Activated");
+    }
+}
